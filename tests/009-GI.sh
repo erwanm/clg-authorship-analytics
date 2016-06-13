fb2b124da2e4071918f1615f756fd15da03e1bd2
Erwan Moreau <moreaue@scss.tcd.ie>
2016-06-13 16:51:23 +0100

    new test

---
+++ tests/009-GI.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+scoresDir="/tmp/scores-tables"
+if [ $# -eq 1 ]; then
+    if [ -f "$1" ]; then
+	logOpt="-l $1"
+	echo "$0 info: logging config file = $1"
+    else
+	logOpt="-l $1"
+	echo "$0 info: logging level = $1"
+    fi
+else
+    echo "$0 info: no logging"
+fi
+if [ ! -d "$scoresDir" ]; then
+    mkdir "$scoresDir"
+    if [ $? -ne 0 ]; then
+	echo "Error cannot create dir '$scoresDir'" 1>&2
+    fi
+fi
+echo "$0 info: printing scores tables to dir '$scoresDir'"
+eval "verif-author.pl -i rw $logOpt -p $scoresDir -d tests/data/pan14.impostors tests/009-GI.conf tests/data/english-20-cases/EN001/known01.txt tests/data/english-20-cases/EN001/unknown.txt"
+
